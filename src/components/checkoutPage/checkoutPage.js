1baed23e673da31777a4cdfbc97392a2d13587c5
dipesh shakya <dipeshshakya106@gmail.com>
2020-08-07 18:54:21 +1000

    review order and checkout copmlete

--- src/components/checkoutPage/checkoutPage.js
+++ src/components/checkoutPage/checkoutPage.js
@@ -11,7 +11,18 @@ import { Formik, Form } from "formik";
 
 const steps = ["Shipping address", "Payment details", "Review your order"];
 // const { formId, formField } = checkoutFormModel;
-function getStepContent(step)
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return "Shipping address";
+    case 1:
+      return "Payment details";
+    case 2:
+      return "Review Order";
+    default:
+      return <div> Not Found</div>;
+  }
+}
 function checkoutPage() {
   return <div></div>;
 }
