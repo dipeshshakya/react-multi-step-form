c30016bec3ced6239258059437b2bccabcd03c11
dipesh shakya <dipeshshakya106@gmail.com>
2020-08-07 17:49:45 +1000

    global theme style using themeprovider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import MaterialLayout from "./components/Layout/MaterialLayout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <MaterialLayout />
     </div>
   );
 }
